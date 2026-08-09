0ae0b864da5740dcdb192e5c99cb001bf5cc129d
Dhinakaran Sekar <dhinakaran15022000@gmail.com>
2026-08-09 13:33:33 +0530

    server issue fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:443',
+        target: 'http://0.0.0.1:443',
         changeOrigin: true,
       }
     }
